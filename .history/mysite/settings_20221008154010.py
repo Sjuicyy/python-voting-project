2d60e2876db019d45daa716445bcdb0853c16174
SHYAM JOSHI <sjuicyygmail.com@SAMMYs-MacBook-Air.local>
2022-10-10 11:32:29 +0545

    new

--- .history/mysite/settings_20221008154010.py
+++ .history/mysite/settings_20221008154010.py
@@ -31,8 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
-    'django.contrib.admin',
+'polls.apps.PollsConfig',    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- .history/mysite/settings_20221008154017.py
+++ .history/mysite/settings_20221008154017.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'polls.apps.MembersConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
